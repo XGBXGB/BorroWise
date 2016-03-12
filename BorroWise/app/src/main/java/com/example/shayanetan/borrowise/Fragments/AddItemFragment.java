3d87ec8154c253687423c7938ceac10f5ac7baa1
Shark <shayane_tan@yahoo.com>
2016-03-12 20:33:54 +0800

    Fix conflicts with AddItem and AddMoney

--- BorroWise/app/src/main/java/com/example/shayanetan/borrowise/Fragments/AddItemFragment.java
+++ BorroWise/app/src/main/java/com/example/shayanetan/borrowise/Fragments/AddItemFragment.java
@@ -3,41 +3,23 @@
  * Created by ShayaneTan on 3/11/2016.
  */
 package com.example.shayanetan.borrowise.Fragments;
-import android.app.Activity;
-import android.content.res.Resources;
-import android.database.Cursor;
-import android.graphics.Color;
+
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.FilterQueryProvider;
-import android.widget.ImageButton;
-import android.widget.Spinner;
 
-import com.example.shayanetan.borrowise.Adapters.ContactsCursorAdapter;
-import com.example.shayanetan.borrowise.MainActivity;
-import com.example.shayanetan.borrowise.Models.DatabaseOpenHelper;
+import android.widget.ImageButton;
+
 import com.example.shayanetan.borrowise.Models.ItemTransaction;
 import com.example.shayanetan.borrowise.Models.User;
 import com.example.shayanetan.borrowise.R;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class AddItemFragment extends AddTransactionAbstractFragment {
 
@@ -73,29 +55,6 @@ public class AddItemFragment extends AddTransactionAbstractFragment {
             @Override
             public void onClick(View v) {
 
-<<<<<<< HEAD
-                databaseOpenHelper.insertTransaction(new ItemTransaction("Item",
-                        selected_contactID,
-                        "borrow",
-                        1,
-                        parseDateToMillis(btn_start_date.getText().toString()),
-                        parseDateToMillis(btn_end_date.getText().toString()),
-                        0.0,
-                        et_AIItemName.getText().toString(),
-                        et_AIDescription.getText().toString()));
-
-                System.out.println("USER: " + selected_name + " NUMBER: " + selected_contact_number);
-                if(databaseOpenHelper.checkUserIfExists(selected_name, selected_contact_number) != -1){
-                    System.out.println("USER ALREADY EXISTS!");
-                }else {
-                    System.out.println("USER doesnt EXISTS!");
-                    databaseOpenHelper.insertUser(new User(selected_name, selected_contact_number, 0));
-                }
-=======
-                //Cursor contactDetail = (Cursor) spnr_AIPersonName.getSelectedItem();
-               // String name = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                //String number = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-
                 int id = databaseOpenHelper.checkUserIfExists(selected_name, selected_contact_number);
                 if(id == -1){
                     id = (int) databaseOpenHelper.insertUser(new User(selected_name, selected_contact_number, 0));
@@ -109,35 +68,13 @@ public class AddItemFragment extends AddTransactionAbstractFragment {
                         parseDateToMillis(btn_start_date.getText().toString()), parseDateToMillis(btn_end_date.getText().toString()),
                         0.0, et_AIItemName.getText().toString(), et_AIDescription.getText().toString()));
 
->>>>>>> origin/master
             }
         });
 
         btn_lent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Cursor contactDetail = (Cursor) spnr_AIPersonName.getSelectedItem();
-                // String name = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                //String number = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 
-<<<<<<< HEAD
-                databaseOpenHelper.insertTransaction(
-                        new ItemTransaction("Item", selected_contactID, "lend", 1,
-                                parseDateToMillis(btn_start_date.getText().toString()),
-                                parseDateToMillis(btn_end_date.getText().toString()),
-                                0.0,
-                                et_AIItemName.getText().toString(),
-                                et_AIDescription.getText().toString()));
-
-                System.out.println("USER: " + selected_name + " NUMBER: " + selected_contact_number);
-
-                if (databaseOpenHelper.checkUserIfExists(selected_name, selected_contact_number) != -1) {
-                    System.out.println("USER ALREADY EXISTS!");
-                } else {
-                    System.out.println("USER doesnt EXISTS!");
-                    databaseOpenHelper.insertUser(new User(selected_name, selected_contact_number, 0));
-                }
-=======
                 int id = databaseOpenHelper.checkUserIfExists(selected_name, selected_contact_number);
                 if(id == -1){
                     id = (int) databaseOpenHelper.insertUser(new User(selected_name, selected_contact_number, 0));
@@ -150,10 +87,6 @@ public class AddItemFragment extends AddTransactionAbstractFragment {
                         "Item", id, "lend", 0,
                         parseDateToMillis(btn_start_date.getText().toString()), parseDateToMillis(btn_end_date.getText().toString()),
                         0.0, et_AIItemName.getText().toString(), et_AIDescription.getText().toString()));
-
-
-
->>>>>>> origin/master
             }
         });
 
--- BorroWise/app/src/main/java/com/example/shayanetan/borrowise/Fragments/AddMoneyFragment.java
+++ BorroWise/app/src/main/java/com/example/shayanetan/borrowise/Fragments/AddMoneyFragment.java
@@ -1,15 +1,11 @@
 package com.example.shayanetan.borrowise.Fragments;
 
-import android.app.Activity;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,7 +14,6 @@ import android.widget.ImageButton;
 import com.example.shayanetan.borrowise.Models.MoneyTransaction;
 import com.example.shayanetan.borrowise.Models.User;
 import com.example.shayanetan.borrowise.R;
-
 
 
 public class AddMoneyFragment extends AddTransactionAbstractFragment {
@@ -57,65 +52,25 @@ public class AddMoneyFragment extends AddTransactionAbstractFragment {
         btn_borrowed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Cursor contactDetail = (Cursor) spnr_AIPersonName.getSelectedItem();
-                // String name = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                //String number = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 
-<<<<<<< HEAD
-                databaseOpenHelper.insertTransaction(new MoneyTransaction(
-                        "Money",selected_contactID, "borrow", 1,
-                        parseDateToMillis(btn_start_date.getText().toString()),
-                        parseDateToMillis(btn_end_date.getText().toString()),
-                        0.0,
-                        Double.parseDouble(et_AMAmount.getText().toString()), 0.0));
-
-                if(databaseOpenHelper.checkUserIfExists(selected_name, selected_contact_number) != -1){
-                    System.out.println("USER ALREADY EXISTS!");
-                }else{
-                    System.out.println("USER doesnt EXISTS!");
-                    databaseOpenHelper.insertUser(new User(selected_name, selected_contact_number, 0));
-                }
-
-=======
                 int id = databaseOpenHelper.checkUserIfExists(selected_name, selected_contact_number);
                 if(id == -1){
                     id = (int) databaseOpenHelper.insertUser(new User(selected_name, selected_contact_number, 0));
                 }else{
                     System.out.println("USER doesnt EXISTS!");
                 }
-
 
                 databaseOpenHelper.insertTransaction(new MoneyTransaction(
                         "Money", id, "borrow", 0,
                         parseDateToMillis(btn_start_date.getText().toString()), parseDateToMillis(btn_end_date.getText().toString()),
                         0.0, Double.parseDouble(et_AMAmount.getText().toString()), 0.0));
-
->>>>>>> origin/master
-            }
+                }
         });
 
         btn_lent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Cursor contactDetail = (Cursor) spnr_AIPersonName.getSelectedItem();
-                // String name = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                //String number = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 
-<<<<<<< HEAD
-                databaseOpenHelper.insertTransaction(new MoneyTransaction(
-                        "Money", selected_contactID, "lend", 1,
-                        parseDateToMillis(btn_start_date.getText().toString()),
-                        parseDateToMillis(btn_end_date.getText().toString()),
-                        0.0,
-                        Double.parseDouble(et_AMAmount.getText().toString()), 0.0));
-
-                if (databaseOpenHelper.checkUserIfExists(selected_name, selected_contact_number) != -1) {
-                    System.out.println("USER ALREADY EXISTS!");
-                } else {
-                    System.out.println("USER doesnt EXISTS!");
-                    databaseOpenHelper.insertUser(new User(selected_name, selected_contact_number, 0));
-                }
-=======
                 int id = databaseOpenHelper.checkUserIfExists(selected_name, selected_contact_number);
                 if(id == -1){
                     id = (int) databaseOpenHelper.insertUser(new User(selected_name, selected_contact_number, 0));
@@ -123,13 +78,10 @@ public class AddMoneyFragment extends AddTransactionAbstractFragment {
                     System.out.println("USER doesnt EXISTS!");
                 }
 
-
                 databaseOpenHelper.insertTransaction(new MoneyTransaction(
                         "Money", id, "lend", 0,
                         parseDateToMillis(btn_start_date.getText().toString()), parseDateToMillis(btn_end_date.getText().toString()),
                         0.0, Double.parseDouble(et_AMAmount.getText().toString()), 0.0));
-
->>>>>>> origin/master
             }
         });
 
