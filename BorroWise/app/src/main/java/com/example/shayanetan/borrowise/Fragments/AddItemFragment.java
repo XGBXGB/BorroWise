1f38a9939a0097b603ede5273359e2e541ac7460
XGBXGB <kewpee10@hotmail.com>
2016-03-12 20:10:31 +0800

    Fixed merging issues

--- BorroWise/app/src/main/java/com/example/shayanetan/borrowise/Fragments/AddItemFragment.java
+++ BorroWise/app/src/main/java/com/example/shayanetan/borrowise/Fragments/AddItemFragment.java
@@ -73,42 +73,22 @@ public class AddItemFragment extends AddTransactionAbstractFragment {
             @Override
             public void onClick(View v) {
 
-<<<<<<< HEAD
-                databaseOpenHelper.insertTransaction(new ItemTransaction("Item",
-                        selected_contactID,
-                        "borrow",
-                        1,
-                        parseDateToMillis(btn_start_date.getText().toString()),
-                        parseDateToMillis(btn_end_date.getText().toString()),
-                        0.0,
-                        et_AIItemName.getText().toString(),
-                        et_AIDescription.getText().toString()));
+                //Cursor contactDetail = (Cursor) spnr_AIPersonName.getSelectedItem();
+               // String name = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                //String number = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 
-                System.out.println("USER: "+ selected_name + " NUMBER: "+ selected_contact_number);
-                if(databaseOpenHelper.checkUserIfExists(selected_name, selected_contact_number)){
-                    System.out.println("USER ALREADY EXISTS!");
-                }else{
-                    System.out.println("USER doesnt EXISTS!");
-                    databaseOpenHelper.insertUser(new User(selected_name, selected_contact_number, 0));
-=======
-                Cursor contactDetail = (Cursor) spnr_AIPersonName.getSelectedItem();
-                String name = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                String number = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-
-                int id = dbHelper.checkUserIfExists(name, number);
+                int id = databaseOpenHelper.checkUserIfExists(selected_name, selected_contact_number);
                 if(id == -1){
-                    id = (int) dbHelper.insertUser(new User(name, number, 0));
+                    id = (int) databaseOpenHelper.insertUser(new User(selected_name, selected_contact_number, 0));
                 }else{
                     System.out.println("USER doesnt EXISTS!");
->>>>>>> origin/master
                 }
 
 
-                dbHelper.insertTransaction(new ItemTransaction(
+                    databaseOpenHelper.insertTransaction(new ItemTransaction(
                         "Item", id, "borrow", 0,
-                        parseDateToMillis(et_AIStartDate.getText().toString()), parseDateToMillis(et_AIEndDate.getText().toString()),
+                        parseDateToMillis(btn_start_date.getText().toString()), parseDateToMillis(btn_end_date.getText().toString()),
                         0.0, et_AIItemName.getText().toString(), et_AIDescription.getText().toString()));
-
 
             }
         });
@@ -116,41 +96,25 @@ public class AddItemFragment extends AddTransactionAbstractFragment {
         btn_lent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Cursor contactDetail = (Cursor) spnr_AIPersonName.getSelectedItem();
+                // String name = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                //String number = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 
-<<<<<<< HEAD
-                databaseOpenHelper.insertTransaction(
-                            new ItemTransaction("Item",selected_contactID,"lend",1,
-                                                parseDateToMillis(btn_start_date.getText().toString()),
-                                                parseDateToMillis(btn_end_date.getText().toString()),
-                                                0.0,
-                                                et_AIItemName.getText().toString(),
-                                                et_AIDescription.getText().toString()));
-
-                System.out.println("USER: "+ selected_name + " NUMBER: "+selected_contact_number);
-
-                if(databaseOpenHelper.checkUserIfExists(selected_name, selected_contact_number)){
-                    System.out.println("USER ALREADY EXISTS!");
-                }else{
-                    System.out.println("USER doesnt EXISTS!");
-                    databaseOpenHelper.insertUser(new User(selected_name, selected_contact_number, 0));
-=======
-                Cursor contactDetail = (Cursor) spnr_AIPersonName.getSelectedItem();
-                String name = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                String number = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-
-                int id = dbHelper.checkUserIfExists(name, number);
+                int id = databaseOpenHelper.checkUserIfExists(selected_name, selected_contact_number);
                 if(id == -1){
-                    id = (int) dbHelper.insertUser(new User(name, number, 0));
+                    id = (int) databaseOpenHelper.insertUser(new User(selected_name, selected_contact_number, 0));
                 }else{
                     System.out.println("USER doesnt EXISTS!");
->>>>>>> origin/master
                 }
 
 
-                dbHelper.insertTransaction(new ItemTransaction(
+                databaseOpenHelper.insertTransaction(new ItemTransaction(
                         "Item", id, "lend", 0,
-                        parseDateToMillis(et_AIStartDate.getText().toString()), parseDateToMillis(et_AIEndDate.getText().toString()),
+                        parseDateToMillis(btn_start_date.getText().toString()), parseDateToMillis(btn_end_date.getText().toString()),
                         0.0, et_AIItemName.getText().toString(), et_AIDescription.getText().toString()));
+
+
+
             }
         });
 
--- BorroWise/app/src/main/java/com/example/shayanetan/borrowise/Fragments/AddMoneyFragment.java
+++ BorroWise/app/src/main/java/com/example/shayanetan/borrowise/Fragments/AddMoneyFragment.java
@@ -65,83 +65,45 @@ public class AddMoneyFragment extends AddTransactionAbstractFragment {
         btn_borrowed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Cursor contactDetail = (Cursor) spnr_AIPersonName.getSelectedItem();
+                // String name = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                //String number = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 
-<<<<<<< HEAD
-                databaseOpenHelper.insertTransaction(new MoneyTransaction(
-                        "Money",selected_contactID, "borrow", 1,
-                        parseDateToMillis(btn_start_date.getText().toString()),
-                        parseDateToMillis(btn_end_date.getText().toString()),
-                        0.0,
-                        Double.parseDouble(et_AMAmount.getText().toString()), 0.0));
-
-                if(databaseOpenHelper.checkUserIfExists(selected_name, selected_contact_number)){
-                    System.out.println("USER ALREADY EXISTS!");
-                }else{
-                    System.out.println("USER doesnt EXISTS!");
-                    databaseOpenHelper.insertUser(new User(selected_name, selected_contact_number, 0));
-                }
-=======
-
-                Cursor contactDetail = (Cursor) spnr_AMPersonName.getSelectedItem();
-                String name = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                String number = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-
-                int id = dbHelper.checkUserIfExists(name, number);
+                int id = databaseOpenHelper.checkUserIfExists(selected_name, selected_contact_number);
                 if(id == -1){
-                    id = (int) dbHelper.insertUser(new User(name, number, 0));
+                    id = (int) databaseOpenHelper.insertUser(new User(selected_name, selected_contact_number, 0));
                 }else{
                     System.out.println("USER doesnt EXISTS!");
                 }
 
 
-                dbHelper.insertTransaction(new MoneyTransaction(
+                databaseOpenHelper.insertTransaction(new MoneyTransaction(
                         "Money", id, "borrow", 0,
-                        parseDateToMillis(et_AMStartDate.getText().toString()), parseDateToMillis(et_AMEndDate.getText().toString()),
-                        0.0,
-                        Double.parseDouble(et_AMAmount.getText().toString()), 0.0));
+                        parseDateToMillis(btn_start_date.getText().toString()), parseDateToMillis(btn_end_date.getText().toString()),
+                        0.0, Double.parseDouble(et_AMAmount.getText().toString()), 0.0));
 
-
->>>>>>> origin/master
             }
         });
 
         btn_lent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Cursor contactDetail = (Cursor) spnr_AIPersonName.getSelectedItem();
+                // String name = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                //String number = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 
-<<<<<<< HEAD
-                databaseOpenHelper.insertTransaction(new MoneyTransaction(
-                        "Money",selected_contactID, "lend", 1,
-                        parseDateToMillis(btn_start_date.getText().toString()),
-                        parseDateToMillis(btn_end_date.getText().toString()),
-                        0.0,
-                        Double.parseDouble(et_AMAmount.getText().toString()), 0.0));
-
-                if(databaseOpenHelper.checkUserIfExists(selected_name, selected_contact_number)){
-                    System.out.println("USER ALREADY EXISTS!");
-                }else{
-                    System.out.println("USER doesnt EXISTS!");
-                    databaseOpenHelper.insertUser(new User(selected_name, selected_contact_number, 0));
-=======
-                Cursor contactDetail = (Cursor) spnr_AMPersonName.getSelectedItem();
-                String name = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                String number = contactDetail.getString(contactDetail.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-
-                int id = dbHelper.checkUserIfExists(name, number);
+                int id = databaseOpenHelper.checkUserIfExists(selected_name, selected_contact_number);
                 if(id == -1){
-                    id = (int) dbHelper.insertUser(new User(name, number, 0));
+                    id = (int) databaseOpenHelper.insertUser(new User(selected_name, selected_contact_number, 0));
                 }else{
                     System.out.println("USER doesnt EXISTS!");
->>>>>>> origin/master
                 }
 
 
-                dbHelper.insertTransaction(new MoneyTransaction(
+                databaseOpenHelper.insertTransaction(new MoneyTransaction(
                         "Money", id, "lend", 0,
-                        parseDateToMillis(et_AMStartDate.getText().toString()), parseDateToMillis(et_AMEndDate.getText().toString()),
-                        0.0,
-                        Double.parseDouble(et_AMAmount.getText().toString()), 0.0));
-
+                        parseDateToMillis(btn_start_date.getText().toString()), parseDateToMillis(btn_end_date.getText().toString()),
+                        0.0, Double.parseDouble(et_AMAmount.getText().toString()), 0.0));
 
             }
         });
